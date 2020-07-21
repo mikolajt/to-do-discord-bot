e0f8df95cc557abcd652b64b0a7d65ec8fa05a31
Mikołaj Topolski <mikolajtopolski@gmail.com>
2020-07-21 22:30:15 +0200

    creating lists

--- bot.js
+++ bot.js
@@ -1,16 +1,50 @@
-let Discord = require('discord.js');
-let auth = require('./auth.json');
-const client = new Discord.Client();
+const Discord = require('discord.js'),
+      auth = require('./auth.json'),
+      client = new Discord.Client();
 
-client.on('ready', () => {
+let prefix = 'Alfred, ',
+    lists = [];
+
+function toDoList(name)
+{
+    this.name = name;
+    this.tasks = [];
+}
+
+function task(content)
+{
+    this.content = content;
+    this.isCompleted = false;
+}
+
+client.on('ready', () => 
+{
     console.log(`Hello World!\nLogged in as ${client.user.tag}!`);
     client.user.setAvatar(`./resources/images/alfred_avatar.png`);
 });
 
-client.on('message', msg => {
-    if(msg.content === 'ping') {
+client.on('message', msg => 
+{
+
+    if(msg.content === prefix + 'ping') 
+    {
         msg.reply('pong');
     }
+    else if(msg.content.startsWith(prefix + 'create to do list '))
+    {
+        let list_name = msg.content.split('list ')[1];
+        lists.push(new toDoList(list_name));
+        msg.reply('List ' + list_name + ' created, Sir');
+    }
+    else if(msg.content === prefix + 'show me all lists')
+    {
+        msg.reply('All lists, Sir\n')
+        for(let i = 0; i < lists.length; i++) 
+        {
+            msg.reply('[' + (i + 1) + '] ' + lists[i].name + '\n');
+        };
+    }
+    
 });
 
 client.login(auth.token);
