9dd39d9369361d7661cbe76010055ebbacdcc2fe
Mikołaj <mikolajtopolski@gmail.com>
2020-10-13 22:03:03 +0200

    displaying tasks in list and completing task

--- bot.js
+++ bot.js
@@ -34,13 +34,31 @@ client.on('message', msg =>
     {
         msg.reply('ping');
     }
+
     //Creating new list e.g. Alfred, create list [list name]
     else if(msg.content.startsWith(prefix + 'create list '))
     {
         let list_name = msg.content.split('list ')[1];
-        lists.push(new toDoList(list_name));
-        msg.reply('List ' + list_name + ' created, Sir');
+        let isUnique = true;
+        for(let i = 0; i < lists.length; i++) 
+        {
+            if(lists[i].name == list_name)
+            {
+                isUnique = false;
+                break;
+            }
+        };
+        if(isUnique == true)
+        {
+            lists.push(new toDoList(list_name));
+            msg.reply('List ' + list_name + ' created, Sir');
+        }
+        else
+        {
+            msg.reply('List with this name already exists, Sir');
+        }
     }
+
     //Display lists e.g. Alfred, show me all lists
     else if(msg.content === prefix + 'show me all lists')
     {
@@ -50,6 +68,7 @@ client.on('message', msg =>
             msg.reply('[' + (i + 1) + '] ' + lists[i].name + '\n');
         };
     }
+
     //Delete list e.g. Alfred, delete list [list name]
     else if(msg.content.startsWith(prefix + 'delete list '))
     {
@@ -64,13 +83,13 @@ client.on('message', msg =>
         lists.splice(index, i);  
         msg.reply('List ' + list_name + ' deleted, Sir');
     }
+
     //Adding task to list e.g. Alfred, add "[task name]" to [list name]
-    //TODO: deleting "" from task name
     else if (msg.content.startsWith(prefix + 'add "'))
     {
         let list_exists = false;
         let list_name = msg.content.split('to ')[1];
-        let task_name = msg.content.match(/"(.*)"/)//.toString();
+        let task_name = msg.content.match(/"(.*)"/);
         task_name = task_name[0].slice(1, -1);
         for(var i = 0; i < lists.length; i++)
         {
@@ -89,6 +108,74 @@ client.on('message', msg =>
             msg.reply("This list doesn't exists, Sir");
         }
     }
+
+    //Displaying tasks in list e.g Alfred, show me [list name]
+    else if (msg.content.startsWith(prefix + 'show me '))
+    {
+        let list_name = msg.content.split('me ')[1];
+        let exists = false;
+        for(let i = 0; i < lists.length; i++) 
+        {
+            if(list_name === lists[i].name)
+            {
+                exists = true;
+                msg.reply('Tasks from ' + list_name + ', Sir');
+                lists[i].tasks.forEach(task => {
+                    if (task.isCompleted == true)
+                    {
+                        msg.reply(':white_check_mark: ' + task.content);
+                    }
+                    else 
+                    {
+                        msg.reply(':x: ' + task.content);
+                    }
+                });
+                break;
+            }
+        }
+        if (exists == false)
+        {
+           msg.reply('List with given name didin\'t exists, Sir'); 
+        }
+    }
+
+    //Complete task e.g Alfred, complete "[task name]" from [list name]
+    else if(msg.content.startsWith(prefix + 'complete "'))
+    {
+        let list_exists = false;
+        let task_exists = false;
+        let list_name = msg.content.split('from ')[1];
+        let task_name = msg.content.match(/"(.*)"/);
+        task_name = task_name[0].slice(1, -1);
+        for(var i = 0; i < lists.length; i++)
+        {
+            if(lists[i].name == list_name)
+            {
+                list_exists = true;
+                lists[i].tasks.forEach(task => {
+                    if(task.content == task_name)
+                    {
+                        task_exists = true;
+                        task.isCompleted = true;
+                    }
+                });
+            }
+        }
+        if(list_exists && task_exists)
+        {
+            msg.reply('Task ' + task_name + ' completed, Sir' );
+        }
+        else if (!list_exists)
+        {
+            msg.reply("This list doesn't exists, Sir");
+        }
+        else if(!task_exists)
+        {
+            msg.reply("This task doesn't exists, Sir");
+        }
+    }
+
+    //Unkonwn command
     else if (msg.content.startsWith(prefix))
     {
         msg.reply("Sorry, I do not understand, Sir");
