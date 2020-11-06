c9e14631beed55ad7b8c5fb26f3debcc56cac089
Mikołaj Topolski <mikolajtopolski@gmail.com>
2020-11-06 18:32:13 +0100

    deleting task, checking and unchecking tasks, saving and reading from json file

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js'),
       auth = require('./auth.json'),
+      fs = require('fs');
       client = new Discord.Client();
 
 let prefix = 'Alfred, ',
@@ -15,6 +16,39 @@ function task(content)
 {
     this.content = content;
     this.isCompleted = false;
+}
+
+function saveToFile(message)
+{
+    let jsonLists = JSON.stringify(lists);
+    let fileName = 'data/' + message.guild.id + '_list.json';
+
+    fs.writeFile(fileName, jsonLists, function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+    });
+}
+
+//TODO: parse json to lists
+function readFromFile(message, callback)
+{
+    let jsonData;
+        content = '';
+        fileName = 'data/' + message.guild.id + '_list.json';
+    content = fs.readFileSync(fileName, 'utf-8');
+    if(content)
+    {
+        jsonData = JSON.parse(content);
+        lists = [];
+        {
+            jsonData.forEach((e) => {
+                lists.push(e);
+            });
+        }
+    }
 }
 
 client.on('ready', () => 
@@ -34,10 +68,10 @@ client.on('message', msg =>
     {
         msg.reply('ping');
     }
-
-    //Creating new list e.g. Alfred, create list [list name]
+    //Creating new list - Alfred, create list [list name]
     else if(msg.content.startsWith(prefix + 'create list '))
     {
+        readFromFile(msg);
         let list_name = msg.content.split('list ')[1];
         let isUnique = true;
         for(let i = 0; i < lists.length; i++) 
@@ -51,6 +85,7 @@ client.on('message', msg =>
         if(isUnique == true)
         {
             lists.push(new toDoList(list_name));
+            saveToFile(msg);
             msg.reply('List ' + list_name + ' created, Sir');
         }
         else
@@ -58,35 +93,46 @@ client.on('message', msg =>
             msg.reply('List with this name already exists, Sir');
         }
     }
-
-    //Display lists e.g. Alfred, show me all lists
+    //Display lists - Alfred, show me all lists
     else if(msg.content === prefix + 'show me all lists')
     {
+        readFromFile(msg);
+        //console.log(lists[0].name);
         msg.reply('All lists, Sir\n')
         for(let i = 0; i < lists.length; i++) 
         {
             msg.reply('[' + (i + 1) + '] ' + lists[i].name + '\n');
         };
     }
-
-    //Delete list e.g. Alfred, delete list [list name]
+    //Delete list - Alfred, delete list [list name]
     else if(msg.content.startsWith(prefix + 'delete list '))
     {
+        readFromFile(msg);
+        let exists = false;
         let list_name = msg.content.split('list ')[1];
         for(var i = 0; i < lists.length; i++)
         {
             if(lists[i].name == list_name)
             {
+                exists = true;
                 break;
             }
         }
-        lists.splice(index, i);  
-        msg.reply('List ' + list_name + ' deleted, Sir');
+        if (exists)
+        {
+            lists.splice(i, 1);
+            saveToFile(msg);
+            msg.reply('List ' + list_name + ' deleted, Sir');   
+        }
+        else 
+        {
+            msg.reply("This list doesn't exists, Sir");
+        }
     }
-
-    //Adding task to list e.g. Alfred, add "[task name]" to [list name]
+    //Adding task to list - Alfred, add "[task name]" to [list name]
     else if (msg.content.startsWith(prefix + 'add "'))
     {
+        readFromFile(msg);
         let list_exists = false;
         let list_name = msg.content.split('to ')[1];
         let task_name = msg.content.match(/"(.*)"/);
@@ -101,6 +147,7 @@ client.on('message', msg =>
         }
         if(list_exists)
         {
+            saveToFile(msg);
             msg.reply('Task ' + task_name + ' added to list ' + list_name + ', Sir' );
         }
         else 
@@ -108,10 +155,10 @@ client.on('message', msg =>
             msg.reply("This list doesn't exists, Sir");
         }
     }
-
-    //Displaying tasks in list e.g Alfred, show me [list name]
+    //Displaying tasks in list - Alfred, show me [list name]
     else if (msg.content.startsWith(prefix + 'show me '))
     {
+        readFromFile(msg);
         let list_name = msg.content.split('me ')[1];
         let exists = false;
         for(let i = 0; i < lists.length; i++) 
@@ -138,12 +185,13 @@ client.on('message', msg =>
            msg.reply('List with given name didin\'t exists, Sir'); 
         }
     }
-
-    //Complete task e.g Alfred, complete "[task name]" from [list name]
-    else if(msg.content.startsWith(prefix + 'complete "'))
+    //Check task - Alfred, check "[task name]" from [list name]
+    else if(msg.content.startsWith(prefix + 'check "'))
     {
+        readFromFile(msg);
         let list_exists = false;
         let task_exists = false;
+        let changed = false;
         let list_name = msg.content.split('from ')[1];
         let task_name = msg.content.match(/"(.*)"/);
         task_name = task_name[0].slice(1, -1);
@@ -155,15 +203,27 @@ client.on('message', msg =>
                 lists[i].tasks.forEach(task => {
                     if(task.content == task_name)
                     {
+                        if(!task.isCompleted)
+                        {
+                            changed = true;
+                            task.isCompleted = true;
+                        }
                         task_exists = true;
-                        task.isCompleted = true;
                     }
                 });
             }
         }
         if(list_exists && task_exists)
         {
-            msg.reply('Task ' + task_name + ' completed, Sir' );
+            if(changed)
+            {
+                saveToFile(msg);
+                msg.reply('Task ' + task_name + ' checked, Sir' );
+            }
+            else
+            {
+                msg.reply('This task is already checked, Sir');
+            }
         }
         else if (!list_exists)
         {
@@ -174,11 +234,105 @@ client.on('message', msg =>
             msg.reply("This task doesn't exists, Sir");
         }
     }
-
+    //Uncheck task - Alfred, uncheck "[task_name]" from [list_name]
+    else if(msg.content.startsWith(prefix + 'uncheck "'))
+    {
+        readFromFile(msg);
+        let list_exists = false;
+        let task_exists = false;
+        let changed = false;
+        let list_name = msg.content.split('from ')[1];
+        let task_name = msg.content.match(/"(.*)"/);
+        task_name = task_name[0].slice(1, -1);
+        for(var i = 0; i < lists.length; i++)
+        {
+            if(lists[i].name == list_name)
+            {
+                list_exists = true;
+                lists[i].tasks.forEach(task => {
+                    if(task.content == task_name)
+                    {
+                        if(task.isCompleted)
+                        {
+                            task.isCompleted = false;
+                            changed = true;
+                        }
+                        task_exists = true;
+                    }
+                });
+            }
+        }
+        if(list_exists && task_exists)
+        {
+            if(changed)
+            {
+                saveToFile(msg);
+                msg.reply('Task ' + task_name + ' unchecked, Sir' );
+            }
+            else
+            {
+                msg.reply('This task is already unchecked, Sir');
+            }
+        }
+        else if (!list_exists)
+        {
+            msg.reply("This list doesn't exists, Sir");
+        }
+        else if(!task_exists)
+        {
+            msg.reply("This task doesn't exists, Sir");
+        }
+    }
+    //Delete task from list - Alfred. delete "[task_name]" from [list_name]
+    else if(msg.content.startsWith(prefix + 'delete "'))
+    {
+        readFromFile(msg);
+        let list_exists = false;
+        let task_exists = false;
+        let list_name = msg.content.split('from ')[1];
+        let task_name = msg.content.match(/"(.*)"/);
+        task_name = task_name[0].slice(1, -1);
+        loop1:
+        for(var i = 0; i < lists.length; i++)
+        {
+            if(lists[i].name == list_name)
+            {
+                list_exists = true;
+                for(var j = 0; j < lists[i].tasks.length; j++)
+                {
+                    if(lists[i].tasks[j].content == task_name)
+                    {
+                        task_exists = true;
+                        break loop1;
+                    }
+                }
+            }
+        }
+        if(list_exists && task_exists)
+        {
+            lists[i].tasks.splice(j, 1);
+            saveToFile(msg);
+            msg.reply('Task ' + task_name + ' deleted, Sir' );
+        }
+        else if (!list_exists)
+        {
+            msg.reply("This list doesn't exists, Sir");
+        }
+        else if(!task_exists)
+        {
+            msg.reply("This task doesn't exists, Sir");
+        }
+    }
+    //List of commands
+    else if (msg.content == prefix + "how can you help me?")
+    {
+        msg.reply("List of commands, Sir \n\n :green_circle: Creating new list - Alfred, create list [list name] \n\n :green_circle: Delete list - Alfred, delete list [list name] \n\n :green_circle: Display lists - Alfred, show me all lists \n\n :green_circle: Adding task to list - Alfred, add \"[task name]\" to [list name] \n\n :green_circle: Displaying tasks in list - Alfred, show me [list name] \n\n :green_circle: Check task - Alfred, check \"[task name]\" from [list name] \n\n :green_circle: Uncheck task - Alfred, uncheck \"[task_name]\" from [list_name] \n\n :green_circle: Delete task from list - Alfred. delete \"[task_name]\" from [list_name]")
+    }
     //Unkonwn command
     else if (msg.content.startsWith(prefix))
     {
         msg.reply("Sorry, I do not understand, Sir");
+        msg.reply('Type "Alfred, how can you help me?" for list of commands');
     }
 });
 
